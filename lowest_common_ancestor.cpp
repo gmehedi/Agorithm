2fee25faa45f1b8f7e9a7e3520e091ad66093dce
gmehedi <40127136+gmehedi@users.noreply.github.com>
2018-09-04 02:43:26 +0600

    Update lowest_common_ancestor.cpp

--- lowest_common_ancestor.cpp
+++ lowest_common_ancestor.cpp
@@ -37,10 +37,7 @@ void bfs(int s)
 void bfs()
 {
     memset(sign,true,sizeof sign);
-    for(int i=1; i<=n; i++)
-    {
-        if(sign[i]) bfs(i);
-    }
+    for(int i=1; i<=n; i++) if(sign[i]) bfs(i);
 }
 
 void build_table()
@@ -50,9 +47,7 @@ void build_table()
         for(int i=1; i<=n; i++) //node
         {
             if(p==0) { table[p][i]=parent[i]; continue; }
-        //     cout<<"P  "<<table[p-1][i]<<endl;
             if( table[p-1][i] != -1 ) table[p][i]= table[p-1][table[p-1][i]];
-
         }
     }
 }
@@ -76,8 +71,8 @@ int Lowest_common_ancestor(int u, int v)
         if( u1 != v1)  u=u1, v=v1;
     }
     return parent[u];
-
 }
+
 int main()
 {
     scanf("%d %d",&n,&m);
@@ -91,10 +86,9 @@ int main()
     bfs();
     //for(int i=1; i<=n; i++) cout<<"P  "<<i<<"   "<<parent[i]<<"  "<<level[i]<<endl;
     build_table();
-
+    
     int q;
     scanf("%d",&q);
-
     for(int i=0; i<q; i++)
     {
         int u,v;
