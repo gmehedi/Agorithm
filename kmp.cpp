73c2e83abd2975d497f7f2ad4e56319d4efaf419
gmehedi <40127136+gmehedi@users.noreply.github.com>
2018-09-07 07:00:57 +0600

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -1,13 +1,14 @@
 //M M Mehedi Hasan
 //From BUBT
-#include <bits/stdc++.h>
-using namespace std;
 
-int* prefix_kmp(string &s2)
+#include <bits/stdc++.h>
+#define szz 2010
+using namespace std;
+int phi[szz];
+int prefix_kmp(string &s2)
 {
     int j=0;
     int len= (int)s2.size();
-    int* phi=new int[len];
     phi[0]=0;
     for(int i=1; i<len; i++)
     {
@@ -15,21 +16,19 @@ int* prefix_kmp(string &s2)
         if(s2[i] == s2[j]) j++;
         phi[i]=j;
     }
-
    // for(int i=0; i<len; i++) cout<<phi[i]<<" ";
-    return phi;
 }
 
 bool kmp(string &s1, string &s2)
 {
-    int* phi= prefix_kmp(s2);
+    prefix_kmp(s2);
     int j=0;
     int len1=(int)s1.size(), len2=(int)s2.size();
     for(int i=0; i<len1 && j<len2; i++)
     {
         while(j>0 && s1[i] != s2[j] ) j=phi[j-1];
         if(s1[i] == s2[j]) j++;
-        if(len2 == j) return true;
+        if(len2 == j) { return true; }
     }
     return false;
 }
@@ -41,8 +40,8 @@ int main()
     for(int i=0; i<q; i++)
     {
         cin>>s2;
-        if( kmp( s1,s2 ) ) printf("Yes\n");
-        else printf("No\n");
+        if( kmp( s1,s2 ) ) printf("Y\n");
+        else printf("N\n");
     }
     return 0;
 }
